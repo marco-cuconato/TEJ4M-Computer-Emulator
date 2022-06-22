ff88b3282086f8b722980c3a9a3d9a6dd2666c23
Ryan <ryan.chung-kam-chung@mths.ca>
2022-06-22 18:35:26 +0000

    Working LDA and STA

--- cpu.py
+++ cpu.py
@@ -23,9 +23,9 @@ class CPU:
         self.program_counter = 0b0000
 
         # Registers
-        self.register_a = 0b0000
-        self.register_b = 0b0000
-        self.register_ir = 0b0000
+        self.register_a = "0b0000"
+        self.register_b = "0b0000"
+        self.register_ir = "0b0000"
 
         # Flags
         self.flag_cf = False
@@ -36,7 +36,7 @@ class CPU:
     def load_program(self):
         """_Opens the file and parses it into an array_"""
         try:
-            with open("main.bin", "rb") as file_content:
+            with open(sys.argv[1], "rb") as file_content:
                 instruction_array = file_content.readlines()
                 file_content.close()
                 print("Starting computer...")
@@ -46,17 +46,13 @@ class CPU:
                     self.memory[line] = instruction_array[line][:8]
                 except IndexError:
                     self.memory[line] = None
-            print(f"Program in memory: {self.memory}")
             self.execute_program()
-        except OSError:
+        except (IndexError, OSError):
             print("Error: Unable to open file.")
             sys.exit()
 
     def execute_program(self):
         """_Executes the program written in the opened file, line by line_
-
-        Args:
-            instruction_array (_dict[str, str]_): _Instructions from the opened file_
 
         Raises:
             ValueError: _description_
@@ -78,43 +74,47 @@ class CPU:
 
         # Iterating line by line of the given main.bin file.
         for line in self.memory:
+            if self.flag_halt:
+                print("Done. Halting Computer...")
+                sys.exit()
+            if self.flag_debug:
+                time.sleep(3)
+            # If debug mode is on, and any of the flags are enabled, print a warning.
+            if self.flag_cf and self.flag_debug:
+                print(f"Warning: Carry out detected in line {self.program_counter}.")
+                self.flag_cf = False
+            elif self.flag_zf and self.flag_debug:
+                print(f"Warning: Zero value detected in line {self.program_counter}.")
+                self.flag_zf = False
+
             if line is not None:
-                self.program_counter += 0b0001
                 try:
                     operator = format(int(line[:4], 2), "#06b")
                     operand = format(int(line[4:9], 2), "#06b")
                     if operator == instruction_set["NOP"]:
                         pass
                     elif operator == instruction_set["LDA"]:
-                        # TODO
-                        pass
+                        self.register_a = self.memory[int(operator, 2)][:4]
                     elif operator == instruction_set["ADD"]:
                         self.register_a = format(
                             int(self.register_a, 2) + int(operand, 2), "#06b"
                         )
-                        if self.flag_debug:
-                            print(
-                                f'Added "A" register with operand {operand}, '
-                                + f"equals to {self.register_a}."
-                            )
+                        self.flag_zf = int(self.register_a, 2) == 0
                         self.flag_cf = int(self.register_a, 2) > 15
                         if self.flag_cf:
-                            self.register_a = self.register_a[:3] + self.register_a[3:]
-                        self.flag_zf = int(self.register_a, 2) == 0
+                            self.register_a = self.register_a[:2] + self.register_a[3:]
+                        if self.flag_debug:
+                            print(f'"A" register value + {operand} = {self.register_a}')
                     elif operator == instruction_set["SUB"]:
                         self.register_a = format(
                             int(self.register_a, 2) - int(operand, 2), "#06b"
                         )
                         if self.flag_debug:
-                            print(
-                                f'Subtracted "A" register with operand {operand}, '
-                                + f"equals to {self.register_a}"
-                            )
+                            print(f'"A" register value - {operand} = {self.register_a}')
                         self.flag_cf = int(self.register_a, 2) < 0
                         self.flag_zf = int(self.register_a, 2) == 0
                     elif operator == instruction_set["STA"]:
-                        # TODO
-                        pass
+                        self.memory[int(operand, 2)] = f"0000{self.register_a[2:]}"
                     elif operator == instruction_set["LDI"]:
                         # Set the register A to the operand
                         self.register_a = operand
@@ -129,7 +129,10 @@ class CPU:
                         print(f"Debug is on: {self.flag_debug}")
                     elif operator == instruction_set["OUT"]:
                         # Output register A to console
-                        print(f'Output: Value of "A" register: {self.register_a}')
+                        if self.flag_cf:
+                            print(f"Output: {self.register_a}")
+                        else:
+                            print(f"Output: {self.register_a}")
                     elif operator == instruction_set["HLT"]:
                         # Halt the program
                         self.flag_halt = True
@@ -139,19 +142,10 @@ class CPU:
                 except ValueError:
                     print(f"Error in line {self.program_counter}!")
                     self.flag_halt = True
+            else:
+                self.flag_debug = True
 
-                if self.flag_halt:
-                    print("Done. Halting Computer...")
-                    sys.exit()
-                if self.flag_debug:
-                    time.sleep(3)
-                # If debug mode is on, and any of the flags are enabled, print a warning.
-                if self.flag_cf and self.flag_debug:
-                    print(f"Warning: Carry out detected in line {self.program_counter}.")
-                    self.flag_cf = False
-                elif self.flag_zf and self.flag_debug:
-                    print(f"Warning: Zero value detected in line {self.program_counter}.")
-                    self.flag_zf = False
+            self.program_counter += 0b0001
 
 
 if __name__ == "__main__":
