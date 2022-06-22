365f4c553a86e4a5eb122feca84fb31ba798e206
Ryan C <ryan.chung-kam-chung@mths.ca>
2022-06-21 22:37:00 -0400

    implementing memory

--- cpu.py
+++ cpu.py
@@ -17,7 +17,8 @@ class CPU:
 
     def __init__(self):
         # RAM
-        self.memory = [0] * 16
+        self.number_of_bytes = 16
+        self.memory = [0b00000000] * self.number_of_bytes
         # Program Counter
         self.program_counter = 0b0000
 
@@ -35,11 +36,18 @@ class CPU:
     def load_program(self):
         """_Opens the file and parses it into an array_"""
         try:
-            with open("main.bin", encoding="utf_8") as file_content:
+            with open("main.bin", "rb") as file_content:
                 instruction_array = file_content.readlines()
                 file_content.close()
                 print("Starting computer...")
-                self.execute_program(instruction_array)
+
+            for line in range(self.number_of_bytes):
+                try:
+                    self.memory[line] = format(int(instruction_array[line][:8], 2), "#010b")
+                except IndexError:
+                    self.memory[line] = None
+            print(f"Program in memory: {self.memory}")
+            self.execute_program(instruction_array)
         except OSError:
             print("Error: Unable to open file.")
             sys.exit()
@@ -70,79 +78,80 @@ class CPU:
 
         # Iterating line by line of the given main.bin file.
         for line in instruction_array:
-            self.program_counter += 0b0001
-            try:
-                operator = format(int(line[:4], 2), "#06b")
-                operand = format(int(line[4:9], 2), "#06b")
-                if operator == instruction_set["NOP"]:
-                    pass
-                elif operator == instruction_set["LDA"]:
-                    # TODO
-                    pass
-                elif operator == instruction_set["ADD"]:
-                    self.register_a = format(
-                        int(self.register_a, 2) + int(operand, 2), "#06b"
-                    )
-                    if self.flag_debug:
-                        print(
-                            f'Added "A" register with operand {operand}, '
-                            + f"equals to {self.register_a}."
+            if line != None:
+                self.program_counter += 0b0001
+                try:
+                    operator = format(int(line[:4], 2), "#06b")
+                    operand = format(int(line[4:9], 2), "#06b")
+                    if operator == instruction_set["NOP"]:
+                        pass
+                    elif operator == instruction_set["LDA"]:
+                        # TODO
+                        pass
+                    elif operator == instruction_set["ADD"]:
+                        self.register_a = format(
+                            int(self.register_a, 2) + int(operand, 2), "#06b"
                         )
-                    self.flag_cf = int(self.register_a, 2) > 15
-                    if self.flag_cf:
-                        self.register_a = self.register_a[:3] + self.register_a[3:]
-                    self.flag_zf = int(self.register_a, 2) == 0
-                elif operator == instruction_set["SUB"]:
-                    self.register_a = format(
-                        int(self.register_a, 2) - int(operand, 2), "#06b"
-                    )
-                    if self.flag_debug:
-                        print(
-                            f'Subtracted "A" register with operand {operand}, '
-                            + f"equals to {self.register_a}"
+                        if self.flag_debug:
+                            print(
+                                f'Added "A" register with operand {operand}, '
+                                + f"equals to {self.register_a}."
+                            )
+                        self.flag_cf = int(self.register_a, 2) > 15
+                        if self.flag_cf:
+                            self.register_a = self.register_a[:3] + self.register_a[3:]
+                        self.flag_zf = int(self.register_a, 2) == 0
+                    elif operator == instruction_set["SUB"]:
+                        self.register_a = format(
+                            int(self.register_a, 2) - int(operand, 2), "#06b"
                         )
-                    self.flag_cf = int(self.register_a, 2) < 0
-                    self.flag_zf = int(self.register_a, 2) == 0
-                elif operator == instruction_set["STA"]:
-                    # TODO
-                    pass
-                elif operator == instruction_set["LDI"]:
-                    # Set the register A to the operand
-                    self.register_a = operand
-                    if self.flag_debug:
-                        print(f'Updated "A" register to {operand}.')
-                elif operator == instruction_set["JMP"]:
-                    # TODO
-                    pass
-                elif operator == instruction_set["DEBUG"]:
-                    # Toggle debug mode
-                    self.flag_debug = not self.flag_debug
-                    print(f"Debug is on: {self.flag_debug}")
-                elif operator == instruction_set["OUT"]:
-                    # Output register A to console
-                    print(f'Output: Value of "A" register: {self.register_a}')
-                elif operator == instruction_set["HLT"]:
-                    # Halt the program
+                        if self.flag_debug:
+                            print(
+                                f'Subtracted "A" register with operand {operand}, '
+                                + f"equals to {self.register_a}"
+                            )
+                        self.flag_cf = int(self.register_a, 2) < 0
+                        self.flag_zf = int(self.register_a, 2) == 0
+                    elif operator == instruction_set["STA"]:
+                        # TODO
+                        pass
+                    elif operator == instruction_set["LDI"]:
+                        # Set the register A to the operand
+                        self.register_a = operand
+                        if self.flag_debug:
+                            print(f'Updated "A" register to {operand}.')
+                    elif operator == instruction_set["JMP"]:
+                        # TODO
+                        pass
+                    elif operator == instruction_set["DEBUG"]:
+                        # Toggle debug mode
+                        self.flag_debug = not self.flag_debug
+                        print(f"Debug is on: {self.flag_debug}")
+                    elif operator == instruction_set["OUT"]:
+                        # Output register A to console
+                        print(f'Output: Value of "A" register: {self.register_a}')
+                    elif operator == instruction_set["HLT"]:
+                        # Halt the program
+                        self.flag_halt = True
+                    else:
+                        print(f"Invalid opcode {operator}.")
+                        raise ValueError
+                except ValueError:
+                    print(f"Error in line {self.program_counter}!")
                     self.flag_halt = True
-                else:
-                    print(f"Invalid opcode {operator}.")
-                    raise ValueError
-            except ValueError:
-                print(f"Error in line {self.program_counter}!")
-                self.flag_halt = True
 
-            if self.flag_halt:
-                print("Done. Halting Computer...")
-                sys.exit()
-            if self.flag_debug:
-                time.sleep(3)
-            # If debug mode is on, and any of the flags are enabled, print a warning.
-            if self.flag_cf and self.flag_debug:
-                print(f"Warning: Carry out detected in line {self.program_counter}.")
-                self.flag_cf = False
-            elif self.flag_zf and self.flag_debug:
-                print(f"Warning: Zero value detected in line {self.program_counter}.")
-                self.flag_zf = False
+                if self.flag_halt:
+                    print("Done. Halting Computer...")
+                    sys.exit()
+                if self.flag_debug:
+                    time.sleep(3)
+                # If debug mode is on, and any of the flags are enabled, print a warning.
+                if self.flag_cf and self.flag_debug:
+                    print(f"Warning: Carry out detected in line {self.program_counter}.")
+                    self.flag_cf = False
+                elif self.flag_zf and self.flag_debug:
+                    print(f"Warning: Zero value detected in line {self.program_counter}.")
+                    self.flag_zf = False
 
 
 if __name__ == "__main__":
